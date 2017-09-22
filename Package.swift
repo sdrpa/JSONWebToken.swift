717632d152cd2613f6de1374cfda30f7452381b4
Sinisa Drpa <sdrpa@tagtaxa.com>
2017-09-22 09:41:38 +0200

    Use IBM-Swift/CommonCrypto

--- Package.swift
+++ Package.swift
@@ -1,24 +1,11 @@
 import PackageDescription
 
-
-#if os(macOS) || os(iOS) || os(watchOS) || os(tvOS)
 let package = Package(
   name: "JWT",
   dependencies: [
-    .Package(url: "https://github.com/kylef-archive/CommonCrypto.git", majorVersion: 1),
+    .Package(url: "https://github.com/IBM-Swift/CommonCrypto.git", majorVersion: 0),
   ],
   exclude: [
     "Sources/HMACCryptoSwift.swift",
   ]
 )
-#else
-let package = Package(
-  name: "JWT",
-  dependencies: [
-    .Package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", majorVersion: 0, minor: 6),
-  ],
-  exclude: [
-    "Sources/HMACCommonCrypto.swift",
-  ]
-)
-#endif
